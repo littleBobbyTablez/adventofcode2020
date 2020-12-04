f8758a8d617f0e87edf4e86fa73f8c791ef082be
Jonas Stendel <jonas.stendel@valtech.com>
2020-12-04 09:23:37 +0100

    add Day4 part two

--- src/main/scala/Day4.scala
+++ src/main/scala/Day4.scala
@@ -3,12 +3,27 @@ object Day4 {
     input.split("\\n\\n").map(_.split("\\s").toList).toList
   }
 
+  val patternsMap = Map("byr" -> "19[2-9][0-9]|200[0-2]", "iyr" -> "201[0-9]|2020", "eyr" -> "202[0-9]|2030",
+    "hgt" -> "1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in", "hcl" -> "#[0-9a-f]{6}",
+    "ecl" -> "amb|blu|brn|gry|grn|hzl|oth", "pid" -> "[0-9]{9}", "cid" ->".*")
 
   val notOptionalFileds = Set("ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt")
 
   def checkValidity(passport: List[String]): Boolean = {
     val real = passport.map(_.split(":")).map(_.head).toSet
     notOptionalFileds.subsetOf(real)
+  }
+
+  def checkValidityOfFields(passport: List[String]): Boolean = {
+    if (checkValidity(passport)) {
+      passport.map(_.split(":")).forall(x => x(1).matches(patternsMap(x(0))))
+    } else {
+      false
+    }
+  }
+
+  def checkPassportsAndFields(list: List[List[String]]): Int = {
+    list.map(checkValidityOfFields).count(_.equals(true))
   }
 
   def checkPassports(list: List[List[String]]): Int = {
--- src/test/scala/Day4Test.scala
+++ src/test/scala/Day4Test.scala
@@ -1,4 +1,4 @@
-import Day4.{checkPassports, transformInput}
+import Day4.{checkPassports, checkPassportsAndFields, transformInput}
 import org.scalatest.FlatSpec
 
 import scala.io.{BufferedSource, Source}
@@ -19,13 +19,20 @@ class Day4Test extends FlatSpec {
     assert(transformInput(input)(0).size.equals(8))
   }
 
+  it should "find valid passports" in {
+
+    val input = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f\n\neyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n\nhcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022\n\niyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719\n\neyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n\niyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946\n\nhcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n\nhgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007"
+
+    assert(checkPassportsAndFields(transformInput(input)).equals(4))
+
+  }
 
   it should "do the thing" in {
 
     val input = Source.fromResource("inputDay4part1.csv").mkString
 
-    print(checkPassports(transformInput(input)))
-
+    println(checkPassports(transformInput(input)))
+    println(checkPassportsAndFields(transformInput(input)))
   }
 
 }
