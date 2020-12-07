1a068a449860c62265b0c93044bb2844e202536a
Jonas Stendel <jonas.stendel@valtech.com>
2020-12-07 10:58:24 +0100

    add Day 7

---
+++ src/main/scala/Day7.scala
@@ -0,0 +1,45 @@
+import scala.annotation.tailrec
+
+object Day7 {
+  def findRequredNumber(input: Map[String, Map[String, Int]], contentColors: Map[String, Int], bags: Int): Int = {
+    val relevant = input.filter(x => contentColors.contains(x._1))
+
+    if(relevant.isEmpty){
+      contentColors.values.toList.sum
+    } else {
+      contentColors.map(col => col._2 + col._2 * findRequredNumber(input, relevant(col._1), 0)).toList.sum
+    }
+
+
+
+  }
+
+  def findOutmostColors(input: Map[String, Map[String, Int]], contentColors: Set[String]): Set[String] = {
+    val set = input.filter(_._2.keys.toSet.intersect(contentColors).nonEmpty).keys.toSet
+
+    if (set.isEmpty) {
+      set
+    } else {
+      set ++ findOutmostColors(input, set)
+    }
+
+
+  }
+
+  def parseRule(input: String): (String, Map[String, Int]) = {
+    if (input.contains("no other")) {
+      (input.split(" ").take(2).mkString(" "), Map.empty)
+    } else {
+      val split = input.split("contain").toList
+
+      val color = split.head.split(" ").take(2).mkString(" ")
+
+      val content: Map[String, Int] = split.last.split(",").toList.map(_.trim)
+        .map(_.split(" ").take(3))
+        .map(x => (x.tail.mkString(" "), x.head.toInt)).toMap
+
+      (color, content)
+    }
+  }
+
+}
---
+++ src/test/scala/Day7Test.scala
@@ -0,0 +1,53 @@
+import Day7.parseRule
+import org.scalatest.FlatSpec
+
+import scala.io.Source
+
+class Day7Test extends FlatSpec {
+
+  it should "parse one rule" in {
+
+    val input = "light red bags contain 1 bright white bag, 2 muted yellow bags."
+    println(parseRule(input))
+  }
+
+
+  it should "parse empty bags" in {
+
+    val input = "faded blue bags contain no other bags."
+    println(parseRule(input))
+  }
+
+
+  it should "find gold bags" in {
+
+    val input = Source.fromResource("exampleInputDay7.csv").getLines().toList.map(parseRule).toMap
+
+    val contentColor = Set("shiny gold")
+    assert(Day7.findOutmostColors(input, contentColor).equals(Set("bright white", "muted yellow", "dark orange", "light red")))
+
+  }
+
+  it should "find containing bags" in {
+    val input = Source.fromResource("inputDay7part1.csv").getLines().toList.map(parseRule).toMap
+
+    val i = Day7.findRequredNumber(input, Map("shiny gold" -> 1), 0)
+    println(i)
+//    assert(i.equals(32))
+
+  }
+
+
+  it should "do the thing" in {
+
+    val input = Source.fromResource("inputDay7part1.csv").getLines().toList.filterNot(_.contains("no other")).map(parseRule).toMap
+
+    val contentColor = Set("shiny gold")
+    print(Day7.findOutmostColors(input, contentColor).size)
+
+  }
+
+
+
+
+}
